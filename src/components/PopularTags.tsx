f1ef8f6fd154bd216c01da52597dbb89c067d5d1
Suraj  Sonkar <surajvirendrasonkar@gmail.com>
2025-07-20 01:55:37 +0530

    completed the populartags component

--- src/components/PopularTags.tsx
+++ src/components/PopularTags.tsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import React from 'react';
+import Image from './Image';
 
 const PopularTags = () => {
-  return (
-    <div>PopularTags</div>
-  )
-}
+	return (
+		<div className="p-4 rounded-2xl border-[1px] border-borderGray flex flex-col gap-4">
+			<h1 className="text-xl font-bold text-textGrayLight">What's Happeing</h1>
+			<div className="flex gap-4">
+				<div className="relative w-20 rounded-xl overflow-hidden">
+					<Image
+						path="general/post.jpeg"
+						alt="event"
+						width={120}
+						height={120}
+					/>
+				</div>
+				<div className="flex-1">
+					<h2 className="font-bold text-textGrayLight">
+						Himalaya tracking season
+					</h2>
+					<span className="text-sm text-textGray">Next Month</span>
+				</div>
+			</div>
+			<div className="">
+				<div className="flex items-center justify-between">
+					<span className='text-textGray text-sm'>Technology + Trending</span>
+					<Image path="icons/infoMore.svg" alt="" width={16} height={16} />
+				</div>
+                    <h2 className='text-textGrayLight font-bold'>OpenAI</h2>
+                    <span className='text-textGray text-sm'>20k posts</span>
+			</div>
+			<div className="">
+				<div className="flex items-center justify-between">
+					<span className='text-textGray text-sm'>Technology + Trending</span>
+					<Image path="icons/infoMore.svg" alt="" width={16} height={16} />
+				</div>
+                    <h2 className='text-textGrayLight font-bold'>OpenAI</h2>
+                    <span className='text-textGray text-sm'>20k posts</span>
+			</div>
+			<div className="">
+				<div className="flex items-center justify-between">
+					<span className='text-textGray text-sm'>Technology + Trending</span>
+					<Image path="icons/infoMore.svg" alt="" width={16} height={16} />
+				</div>
+                    <h2 className='text-textGrayLight font-bold'>OpenAI</h2>
+                    <span className='text-textGray text-sm'>20k posts</span>
+			</div>
+			<div className="">
+				<div className="flex items-center justify-between">
+					<span className='text-textGray text-sm'>Technology + Trending</span>
+					<Image path="icons/infoMore.svg" alt="" width={16} height={16} />
+				</div>
+                    <h2 className='text-textGrayLight font-bold'>OpenAI</h2>
+                    <span className='text-textGray text-sm'>20k posts</span>
+			</div>
+		</div>
+	);
+};
 
-export default PopularTags
+export default PopularTags;
