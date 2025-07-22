b6fb887e1bb8e3cb331d1f6b6881f36aed3a37a6
Suraj  Sonkar <surajvirendrasonkar@gmail.com>
2025-07-22 21:52:51 +0530

    seeded the dummy data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,7 @@ import Share from '@/components/Share';
 import { prisma } from '@/prisma';
 import Link from 'next/link';
 
-const Homepage = async() => {
-
-	const users = await prisma.user.findMany()
-	console.log(users)
-
+const Homepage = () => {
 	return (
 		<div className="">
 			<div className="px-4 pt-4 flex justify-between text-textGray font-bold border-b-[1px] border-borderGray">
--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,14 +1,18 @@
-import Post from "./Post"
+import { prisma } from '@/prisma';
+import Post from './Post';
 
-const Feed = () => {
-  return (
-    <div>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-    </div>
-  )
-}
+const Feed = async () => {
+	const posts = await prisma.post.findMany();
+  console.log(posts.length)
+	return (
+		<div className=''>
+			{posts.map((post) => {
+				return <div key={post.id}>
+					<Post />
+				</div>;
+			})}
+		</div>
+	);
+};
 
-export default Feed
+export default Feed;
