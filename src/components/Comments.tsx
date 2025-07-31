181a801f0e68bbd7431054dd1fb5f0c5594e42a1
Suraj  Sonkar <surajvirendrasonkar@gmail.com>
2025-07-31 15:27:14 +0530

    finished

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,11 +1,12 @@
-"use client"
+'use client';
 
 import { useUser } from '@clerk/nextjs';
 import Image from './Image';
 import Post from './Post';
 import { Post as PostType } from '@prisma/client';
-import { useActionState } from 'react';
+import { useActionState, useEffect } from 'react';
 import { addComment } from '@/action';
+import { socket } from '@/socket';
 
 type commentWithDetails = PostType & {
 	user: { displayName: string | null; username: string; img: string | null };
@@ -26,13 +27,33 @@ const Comments = ({
 }) => {
 	const { isLoaded, isSignedIn, user } = useUser();
 
-	const [state,formAction, isPending] = useActionState(addComment, {success:false,error:false})
+	const [state, formAction, isPending] = useActionState(addComment, {
+		success: false,
+		error: false,
+	});
+
+	// if (!user) return;
+	useEffect(() => {
+		if (state.success) {
+			socket.emit('sendNotification', {
+				receiverUsername: username,
+				data: {
+					senderUsername: user?.username,
+					type: 'comment',
+					link: `/${username}/status/${postId}`,
+				},
+			});
+		}
+	}, [state.success, username, user?.username, postId]);
 	return (
 		<div>
 			{user && (
-				<form action={formAction} className="flex items-center justify-between gap-4 p-4">
+				<form
+					action={formAction}
+					className="flex items-center justify-between gap-4 p-4"
+				>
 					<div className="flex justify-center items-center gap-4">
-						<div className="relative w-10 h-10 rounded-full overflow-hidden">
+						<div className="-z-10 relative w-10 h-10 rounded-full overflow-hidden">
 							<Image
 								src={user?.imageUrl}
 								alt="suraj sonkar"
@@ -41,36 +62,33 @@ const Comments = ({
 								tr={true}
 							/>
 						</div>
+						<input name="postId" type="number" hidden readOnly value={postId} />
 						<input
-							name='postId'
-							type="number"
-							hidden
-							readOnly
-							value={postId}
-						/>
-						<input
-							name='username'
+							name="username"
 							type="text"
 							hidden
 							readOnly
 							value={username}
 						/>
 						<input
-							name='desc'
+							name="desc"
 							type="text"
 							className="bg-transparent  outline-none p-2 text-xl"
 							placeholder="Post your reply!"
 						/>
 					</div>
-					<button disabled={isPending} className="py-2 px-4 font-bold bg-white text-black rounded-full disabled:cursor-not-allowed disabled:bg-slate-200">
-						{isPending ? "Replying": "Reply"}
+					<button
+						disabled={isPending}
+						className="py-2 px-4 font-bold bg-white text-black rounded-full disabled:cursor-not-allowed disabled:bg-slate-200"
+					>
+						{isPending ? 'Replying' : 'Reply'}
 					</button>
 				</form>
 			)}
 
-			{state.error && <span className='text-red-300 p-4'>
-				Something went wrong!
-				</span>}
+			{state.error && (
+				<span className="text-red-300 p-4">Something went wrong!</span>
+			)}
 
 			{comments.map((comment) => (
 				<div key={comment.id}>
--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -13,9 +13,9 @@ type NotificationType = {
 
 const Notification = () => {
 	const [notifications, setNotifications] = useState<NotificationType[]>([]);
-    const [open,setOpen] = useState(false)
-    console.log(notifications)
-    console.log(open)
+	const [open, setOpen] = useState(false);
+	console.log(notifications);
+	console.log(open);
 
 	useEffect(() => {
 		socket.on('getNotification', (data: NotificationType) => {
@@ -23,33 +23,56 @@ const Notification = () => {
 		});
 	}, []);
 
-
-    const reset = ()=> {
-        setNotifications([])
-        setOpen(false)
-    }
+	const reset = () => {
+		setNotifications([]);
+		setOpen(false);
+	};
 	return (
 		<div className="relative">
-			<div className="cursor-pointer rounded-full hover:bg-[#181818] flex p-2 items-center gap-4" onClick={()=>setOpen((prev)=> !prev)}>
-				<Image
-					path={`icons/notification.svg`}
-					alt="notification"
-					width={24}
-					height={24}
-				/>
+			<div
+				className="cursor-pointer rounded-full hover:bg-[#181818] flex p-2 items-center gap-4"
+				onClick={() => setOpen((prev) => !prev)}
+			>
+				<div className="relative">
+					<Image
+						path={`icons/notification.svg`}
+						alt="notification"
+						width={24}
+						height={24}
+					/>
+					{notifications.length > 0 && (
+						<div className="absolute -top-4 -right-4 w-6 h-6 bg-iconBlue p-2 rounded-full flex items-center justify-center text-sm">
+							{notifications.length}
+						</div>
+					)}
+				</div>
 				<span className="hidden xxl:inline">Notifications</span>
 			</div>
-            {open && <div className='absolute -right-full p-4 rounded-lg bg-white text-black flex flex-col gap-4 w-max'>
-                <h1 className='text-xl text-textGray'>Notifications</h1>
-                {notifications.map(n=>(
-                    <div className='cursor-pointer' key={n.id}>
-                        <a href={`${n.senderUsername}`}><b>{n.senderUsername}</b></a>{" "}{n.type === "like" ? "liked your post" : n.type === "rePost" ? "reposted your post" : n.type=== "comment" ? "replied on your post" : "followed you"}
-                    </div>
-                ))}
-                <button onClick={reset} className='bg-black text-white p-2 text-sm rounded-lg'>
-                    Mark as read
-                </button>
-            </div>}
+			{open && (
+				<div className="absolute -right-full p-4 rounded-lg bg-white text-black flex flex-col gap-4 w-max">
+					<h1 className="text-xl text-textGray">Notifications</h1>
+					{notifications.map((n) => (
+						<div className="cursor-pointer" key={n.id}>
+							<a href={`${n.senderUsername}`}>
+								<b>{n.senderUsername}</b>
+							</a>{' '}
+							{n.type === 'like'
+								? 'liked your post'
+								: n.type === 'rePost'
+								? 'reposted your post'
+								: n.type === 'comment'
+								? 'replied on your post'
+								: 'followed you'}
+						</div>
+					))}
+					<button
+						onClick={reset}
+						className="bg-black text-white p-2 text-sm rounded-lg"
+					>
+						Mark as read
+					</button>
+				</div>
+			)}
 		</div>
 	);
 };
